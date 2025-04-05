7d6acbd7e4096797fbc747067382279e457ffb25
nik1tera <nik1teraa@gmail.com>
2025-04-05 20:52:14 +0300

    вернул старый путь

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def save_data(data):
 @app.route("/")
 def home():
     data = load_data()
-    return render_template('templates/index.html')  # Новый путь
+    return render_template('templates')  # Новый путь
 
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
